f3b576db918d897c808e699d01f8363ecda88243
DanieleSalvi2810 <daniele.salvi@mail.polimi.it>
2025-10-12 17:56:51 +0200

    samll error fixed in the computation of g

--- main.c
+++ main.c
@@ -218,17 +218,17 @@ void comp_g(const DTYPE *f_x,
 
                 size_t idx = rowmaj_idx(i, j, k, height, width);
 
-                g_x[idx] = f_x[idx] * grad_x[idx] * ((-2 * nu_const) / k_const) * speed_x[idx] *
+                g_x[idx] = f_x[idx] - grad_x[idx] - ((2 * nu_const) / k_const) * speed_x[idx] +
                              (nu_const / 2) * ((eta_x[rowmaj_idx(i + 1, j, k, height, width)] -2 * eta_x[idx] +
                                                  eta_x[rowmaj_idx(i-1, j, k, height, width)]) * (zeta_x[rowmaj_idx(i + 1, j, k, height, width)] -2 * zeta_x[idx] +
                                                  zeta_x[rowmaj_idx(i-1, j, k, height, width)]) + (speed_x[rowmaj_idx(i + 1, j, k, height, width)] -2 * speed_x[idx] +
                                                  speed_x[rowmaj_idx(i-1, j, k, height, width)]));
-                g_y[idx] = f_y[idx] * grad_y[idx] * ((-2 * nu_const) / k_const) * speed_y[idx] *
+                g_y[idx] = f_y[idx] - grad_y[idx] - ((2 * nu_const) / k_const) * speed_y[idx] *
                              (nu_const / 2) * ((eta_y[rowmaj_idx(i + 1, j, k, height, width)] -2 * eta_y[idx] +
                                                  eta_y[rowmaj_idx(i-1, j, k, height, width)]) * (zeta_y[rowmaj_idx(i + 1, j, k, height, width)] -2 * zeta_y[idx] +
                                                  zeta_y[rowmaj_idx(i-1, j, k, height, width)]) + (speed_y[rowmaj_idx(i + 1, j, k, height, width)] -2 * speed_y[idx] +
                                                  speed_y[rowmaj_idx(i-1, j, k, height, width)]));
-                g_z[idx] = f_z[idx] * grad_z[idx] * ((-2 * nu_const) / k_const) * speed_z[idx] *
+                g_z[idx] = f_z[idx] - grad_z[idx] - ((2 * nu_const) / k_const) * speed_z[idx] *
                              (nu_const / 2) * ((eta_z[rowmaj_idx(i + 1, j, k, height, width)] -2 * eta_z[idx] +
                                                  eta_z[rowmaj_idx(i-1, j, k, height, width)]) * (zeta_z[rowmaj_idx(i + 1, j, k, height, width)] -2 * zeta_z[idx] +
                                                  zeta_z[rowmaj_idx(i-1, j, k, height, width)]) + (speed_z[rowmaj_idx(i + 1, j, k, height, width)] -2 * speed_z[idx] +
