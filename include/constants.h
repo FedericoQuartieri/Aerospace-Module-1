6adc8c2c4c4c9ba2970b690e075cab708a0082a9
DanieleSalvi2810 <daniele.salvi@mail.polimi.it>
2025-10-16 15:51:57 +0200

    added some simple and trivial tests for pressure and g

--- include/constants.h
+++ include/constants.h
@@ -26,8 +26,14 @@
 
 // Numerical parameters
 #define DX 0.1                // Grid spacing in x
+#define DX_INVERSE (1.0 / DX) // Precomputed inverse of DX
+#define DX_INVERSE_SQUARED (DX_INVERSE * DX_INVERSE) // Precomputed inverse squared of DX
 #define DY 0.1                // Grid spacing in y
+#define DY_INVERSE (1.0 / DY) // Precomputed inverse of DY
+#define DY_INVERSE_SQUARED (DY_INVERSE * DY_INVERSE) // Precomputed inverse squared of DY
 #define DZ 0.1                // Grid spacing in z
+#define DZ_INVERSE (1.0 / DZ) // Precomputed inverse of DZ
+#define DZ_INVERSE_SQUARED (DZ_INVERSE * DZ_INVERSE) // Precomputed inverse squared of DZ
 #define DT 0.001              // Time step
 
 // Simulation parameters
