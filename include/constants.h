744b571366bd8029d1266f968782b49b632e9a8a
Sterning171177 <giuseppe.starnini@mail.polimi.it>
2025-10-20 10:51:29 +0200

    fixed errors in computing gradient and pressure_test still fails

--- include/constants.h
+++ include/constants.h
@@ -25,16 +25,18 @@
 #define DENSITY 1.0           // Fluid density (rho)
 
 // Numerical parameters
-#define DX 0.1                // Grid spacing in x
-#define DX_INVERSE (1.0 / DX) // Precomputed inverse of DX
-#define DX_INVERSE_SQUARED (DX_INVERSE * DX_INVERSE) // Precomputed inverse squared of DX
-#define DY 0.1                // Grid spacing in y
-#define DY_INVERSE (1.0 / DY) // Precomputed inverse of DY
-#define DY_INVERSE_SQUARED (DY_INVERSE * DY_INVERSE) // Precomputed inverse squared of DY
-#define DZ 0.1                // Grid spacing in z
-#define DZ_INVERSE (1.0 / DZ) // Precomputed inverse of DZ
-#define DZ_INVERSE_SQUARED (DZ_INVERSE * DZ_INVERSE) // Precomputed inverse squared of DZ
+#define DX 0.001                // Grid spacing in x
+#define DY 0.001                // Grid spacing in y
+#define DZ 0.001                // Grid spacing in z
 #define DT 0.001              // Time step
+
+#define DX_INVERSE (1.0) / DX
+#define DY_INVERSE (1.0) / DY
+#define DZ_INVERSE (1.0) / DZ
+
+#define DX_INVERSE_SQUARE (DX_INVERSE * DX_INVERSE)
+#define DY_INVERSE_SQUARE (DY_INVERSE * DY_INVERSE)
+#define DZ_INVERSE_SQUARE (DZ_INVERSE * DZ_INVERSE)
 
 // Simulation parameters
 #define MAX_ITERATIONS 1000
