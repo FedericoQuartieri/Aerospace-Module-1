54b8fbfd8f934b28ec600e1f837d56f8586df831
FedericoQuartieri <federico.quartieri2002@gmail.com>
2025-11-14 12:25:25 +0100

    starting manufactured solution
    Co-authored-by: EmaSeve <EmaSeve@users.noreply.github.com>

--- test/manufactered_solution.cpp
+++ test/manufactered_solution.cpp
@@ -1,21 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../include/constants.h"
+#include <gtest/gtest.h>
+#include "constants.h"
+#include "utils.h"
+
+// Include C headers in extern "C" block for C++ compatibility
+extern "C"
+{
+#include "g_field.h"
+#include "force_field.h"
 #include "velocity_field.h"
 #include "pressure.h"
-#include "force_field.h"
-#include "g_field.h"
-#include "utils.h"
-#include "momentum_system.h"
-#include "solve.h"
+}
 
-/* Solver for the Navier-Stokes-Brinkman equation */
+TEST(GFieldComputationTest, ComplexFullFieldWithViscousTerms)
+{
 
-
-
-int main(){
-
-    // Initialize Force field
     ForceField f_field;
     initialize_force_field(&f_field);
 
@@ -32,18 +30,21 @@ int main(){
     initialize_velocity_field(&Zeta);
     initialize_velocity_field(&U);
 
-    // Set K 
-    DTYPE *K = (DTYPE *) malloc(GRID_SIZE);
+    // Set K
+    DTYPE *K = (DTYPE *)malloc(GRID_SIZE);
     rand_fill(K);
-    
-    DTYPE *Beta = (DTYPE *) malloc(GRID_SIZE);
-    DTYPE *Gamma = (DTYPE *) malloc(GRID_SIZE);
-    for(int k = 0; k < DEPTH; k++){
-        for(int j = 0; j < HEIGHT; j++){
-            for(int i = 0; i < WIDTH; i++){
-                size_t idx = rowmaj_idx(i,j,k);
+
+    DTYPE *Beta = (DTYPE *)malloc(GRID_SIZE);
+    DTYPE *Gamma = (DTYPE *)malloc(GRID_SIZE);
+    for (int k = 0; k < DEPTH; k++)
+    {
+        for (int j = 0; j < HEIGHT; j++)
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                size_t idx = rowmaj_idx(i, j, k);
                 Beta[idx] = 1 + (DT * NU) / (2 * K[idx]);
-                Gamma[idx] = (DT * NU) / ( 2 * Beta[idx]);
+                Gamma[idx] = (DT * NU) / (2 * Beta[idx]);
             }
         }
     }
@@ -52,17 +53,21 @@ int main(){
     GField g_field;
     initialize_g_field(&g_field);
 
-    /** 
-     * Compute G as: 
+    /**
+     * Compute G as:
      *                 [dx]    f_x   - Grad_x(P) - c * U_x + c[ Grad_xx(N_x) + Grad_yy(Z_x) + Grad_zz(U_x)]
      *            G:   [dy] =  f_y   - Grad_y(P) - c * U_y + c[ Grad_xx(N_y) + Grad_yy(Z_y) + Grad_zz(U_y)]
-     *                 [dz]    f_z   - Grad_z(P) - c * U_z + c[ Grad_xx(N_z) + Grad_yy(Z_z) + Grad_zz(U_z)] 
-     * */     
+     *                 [dz]    f_z   - Grad_z(P) - c * U_z + c[ Grad_xx(N_z) + Grad_yy(Z_z) + Grad_zz(U_z)]
+     * */
     compute_g(&g_field, &f_field, &pressure, K, &Eta, &Zeta, &U);
 
+    solve(g_field, f_field, pressure, K, Eta, Zeta, U, Beta, Gamma, frequency = 1);
 
-    solve(g_field, f_field, pressure, K, Eta, Zeta, U, Beta, Gamma, WRITE_FREQUENCY);
-  
+
+    
+
+    
+
     printf("momentum\n");
 
     free(K);
@@ -72,7 +77,4 @@ int main(){
     free_velocity_field(&Zeta);
     free_velocity_field(&U);
     free_g_field(&g_field);
-
-
-    return 0;
 }
