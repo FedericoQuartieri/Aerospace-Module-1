870a6d924187f83b9e9ca489b00a8a846c972ab6
EmanueleSeverino <emanuele.severino@mail.polimi.it>
2025-11-15 16:52:24 +0100

    Add asynchronous VTK writer and IO thread infrastructure

--- include/solve.h
+++ include/solve.h
@@ -1,14 +1,17 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<pthread.h>
+#include <pthread.h>
 #include "utils.h"
 #include "g_field.h"
 #include "velocity_field.h"
 #include "tridiagonal_blocks.h"
 #include "momentum_system.h"
 #include "pressure.h"
+#include <sys/stat.h>
+#include <sys/types.h>
 
+void solve (GField g_field, ForceField f_field, Pressure pressure, DTYPE* K, 
+            VelocityField Eta, VelocityField Zeta, VelocityField U, DTYPE* Beta, 
+            DTYPE* Gamma, int write_frequency);
 
-
-void solve ();
--- src/solve.c
+++ src/solve.c
@@ -1,31 +1,5 @@
 #include "solve.h"
-
-void *io_thread_func(void *arg)
-{
-    while (1) {
-        pthread_mutex_lock(&io_mutex);
-
-        // Wait until solver thread signals new data
-        while (io_pending == 0)
-            pthread_cond_wait(&io_cond, &io_mutex);
-
-        int buf_to_write = io_pending - 1; // convert 1/2 into 0/1
-        io_pending = 0;
-
-        pthread_mutex_unlock(&io_mutex);
-
-        // ---- Write buffer buf_to_write to disk ----
-        char filename[256];
-        sprintf(filename, "solution_%06d.vti", current_time_step());
-
-        write_vti_file(filename,
-                       &U_buf[buf_to_write],
-                       &P_buf[buf_to_write],
-                       Nx, Ny, Nz, dx, dy, dz);
-
-        // After writing, simply continue and wait for next job
-    }
-}
+#include "io_thread.h"
 
 /* 
     Get the pointers to the Velocity struct and swap each pointers inside them
@@ -34,17 +8,11 @@ static void swap_velocity(VelocityField *U, VelocityField *U_next) {
     
     DTYPE *tmp;
     /* v_x */
-    tmp = U->v_x;
-    U->v_x = U_next->v_x;
-    U_next->v_x = tmp;
+    tmp = U->v_x; U->v_x = U_next->v_x; U_next->v_x = tmp;
     /* v_y */
-    tmp = U->v_y;
-    U->v_y = U_next->v_y;
-    U_next->v_y = tmp;
+    tmp = U->v_y; U->v_y = U_next->v_y; U_next->v_y = tmp;
     /* v_z */
-    tmp = U->v_z;
-    U->v_z = U_next->v_z;
-    U_next->v_z = tmp;
+    tmp = U->v_z; U->v_z = U_next->v_z; U_next->v_z = tmp;
 } 
 
 /* 
@@ -53,39 +21,32 @@ static void swap_velocity(VelocityField *U, VelocityField *U_next) {
 static void swap_pressure(Pressure *pressure, Pressure *pressure_next) {
     
     DTYPE *tmp;
-    tmp = pressure->p;
-    pressure->p = pressure_next->p;
-    pressure_next->p = tmp;
+    tmp = pressure->p; pressure->p = pressure_next->p; pressure_next->p = tmp;
 } 
 
-
-void solve (GField g_field, ForceField f_field, Pressure pressure, Pressure pressure_next, DTYPE* K, VelocityField Eta, VelocityField Zeta, VelocityField U, DTYPE* Beta, DTYPE* Gamma, int write_frequency) {
+void solve (GField g_field, ForceField f_field, Pressure pressure, DTYPE* K, VelocityField Eta, VelocityField Zeta, VelocityField U, DTYPE* Beta, DTYPE* Gamma, int write_frequency) {
     
     /* 
         For the seriel implementation, we will use a separate thread to write the .vtk, 
-        using two buffer such that the solver_thread willd copy U and p to that buffer and the Output_thread
-        will write back to disk using that thread.
+        The IO thread implements a ring queue wich (velocity_buffer, pressure_buffer) for each entries,
+        this allows the solver to continue his execution without wasting time in writing into file,
+        the memcpy is infact much faster than writing into a file.
+        However, when the queue is full, the solver wait for the IO thread signal .not_full, slowing down performance
+        The way to avoid that is to drop some frame and continue execution from the solver.
     */
-    /* Write buffers */
-    VelocityField U_buf[2];
-    Pressure P_buf[2];
-    initialize_velocity_field(&U_buf[0]);
-    initialize_velocity_field(&U_buf[1]);
-    initialize_pressure(&P_buf[0]);
-    initialize_pressure(&P_buf[1]);
-    
-    int current_buf = 0;
+
+    IOQueue io_queue;
+    io_queue_init(&io_queue);
     pthread_t io_thread;
-    pthread_mutex_t io_mutex = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t  io_cond  = PTHREAD_COND_INITIALIZER;
 
-    int io_pending = 0; 
-    /* Start the thread */
-    pthread_create(&io_thread, NULL, io_thread_func, NULL);
-    
+    /* Create the IO thread */
+    if(pthread_create(&io_thread, NULL, io_thread_func, &io_queue) != 0) {
+        printf("\nIO thread was not created correctly\n");
+        io_queue_destroy(&io_queue);
+        return;
+    }
+    mkdir("output", 0755);   /* create output/ directory if doesn't exists */
 
-    // For each time step:
-    // declare U_next, Eta_next, Zeta_next, Xi
     VelocityField Eta_next;
     VelocityField Zeta_next;
     VelocityField U_next;
@@ -95,31 +56,44 @@ void solve (GField g_field, ForceField f_field, Pressure pressure, Pressure pres
     initialize_velocity_field(&Zeta_next);
     initialize_velocity_field(&U_next);
 
+    Pressure pressure_next;
+    initialize_pressure(&pressure_next);
+
     for (int t = 0; t < STEPS; t++) {
-        // Compute G at time step n
+        
         solve_momentum_system(U, Eta, Zeta, Xi, g_field, U_next, Eta_next, Zeta_next, Beta, Gamma);
 
-        solve_pressure_system(pressure, pressure_next, U_next);
-
+        // not implemented yet
+        // solve_pressure_system(pressure, pressure_next, U_next);
 
         if (t % write_frequency == 0) {
 
-            int buf = current_buf;
+            pthread_mutex_lock(&io_queue.mutex);
 
-            // 1. Copy current solver fields into the write buffer
-            memcpy(U_buf[buf].v_x, U_next.v_x, GRID_SIZE);
-            memcpy(U_buf[buf].v_y, U_next.v_y, GRID_SIZE);
-            memcpy(U_buf[buf].v_z, U_next.v_z, GRID_SIZE);
-            memcpy(P_buf[buf].p, pressure_next.p, GRID_SIZE);
+            /* If the queue is full, wait... */
+            while (io_queue.count == IO_QUEUE_SIZE) {
+                pthread_cond_wait(&io_queue.not_full, &io_queue.mutex);
+            }
 
-            // 2. Signal the IO thread that a write is pending
-            pthread_mutex_lock(&io_mutex);
-            io_pending = buf+1;  // e.g. 1 means buf 0, 2 means buf 1
-            pthread_cond_signal(&io_cond);
-            pthread_mutex_unlock(&io_mutex);
+            int idx = io_queue.head;
+            /* Update the head of the queue, but wait to increment .count */
+            io_queue.head = (io_queue.head + 1) % IO_QUEUE_SIZE;
 
-            // 3. Switch buffer (ping-pong)
-            current_buf = 1 - current_buf;
+            pthread_mutex_unlock(&io_queue.mutex);
+            
+            io_queue.timestep[idx] = t;
+            /* Here IO thread can continue to work, doesn't wait for memcpy */
+            /* Copy data into buffers at position [head] */
+            memcpy(io_queue.U_buf[idx].v_x, U_next.v_x, GRID_SIZE);
+            memcpy(io_queue.U_buf[idx].v_y, U_next.v_y, GRID_SIZE);
+            memcpy(io_queue.U_buf[idx].v_z, U_next.v_z, GRID_SIZE);
+            memcpy(io_queue.P_buf[idx].p,  pressure_next.p, GRID_SIZE);
+
+            /* Signal that the next buffer is ready, so update .count */
+            pthread_mutex_lock(&io_queue.mutex);
+            io_queue.count++;
+            pthread_cond_signal(&io_queue.not_empty);
+            pthread_mutex_unlock(&io_queue.mutex);
         }
 
         swap_velocity(&U, &U_next);
@@ -133,6 +107,11 @@ void solve (GField g_field, ForceField f_field, Pressure pressure, Pressure pres
     free_velocity_field(&Zeta_next);
     free_velocity_field(&U_next);
 
-    pthread_cancel(io_thread);
+    pthread_mutex_lock(&io_queue.mutex);
+    io_queue.stop = 1;
+    pthread_cond_signal(&io_queue.not_empty);
+    pthread_mutex_unlock(&io_queue.mutex);
+
     pthread_join(io_thread, NULL);
+    io_queue_destroy(&io_queue);
 }
