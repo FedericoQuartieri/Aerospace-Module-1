b19922d2f34266531e077247f750c3086f05eebc
FedericoQuartieri <federico.quartieri2002@gmail.com>
2025-11-24 12:15:47 +0100

    include of pressure system was missing

--- include/solve.h
+++ include/solve.h
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include "io_thread.h"
 #include "utils.h"
 #include "g_field.h"
 #include "velocity_field.h"
 #include "tridiagonal_blocks.h"
 #include "momentum_system.h"
 #include "pressure.h"
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdbool.h>
-#include "io_thread.h"
+#include "pressure_system.h"
 
 void solve (GField g_field, forcing_function_t forcing, Pressure pressure, DTYPE* K, 
             VelocityField Eta, VelocityField Zeta, VelocityField U, 
